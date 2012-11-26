bea044329d72b6f358c07d7822d4d54696cd6dba
Tristan Sloughter <tristan@heroku.com>
2012-11-26 15:17:52 -0600

    add extension to file output

--- opt/serve.rb
+++ opt/serve.rb
@@ -3,5 +3,5 @@ require 'sinatra'
 set :public_folder, File.dirname(__FILE__) + '/dialyzed/'
 
 get '/' do
-  send_file File.join(settings.public_folder, 'output')
+  send_file File.join(settings.public_folder, 'output.html')
 end
